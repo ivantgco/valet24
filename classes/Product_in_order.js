5382b359777b80ce88fe67e09a884f008e6ebbec
ivantgca <ivantgca@gmail.com>
2016-08-10 03:13:58 +0300

    Order create 80%

---
+++ classes/Product_in_order.js
@@ -0,0 +1,55 @@
+/**
+ * Created by iig on 29.10.2015.
+ */
+var MyError = require('../error').MyError;
+var UserError = require('../error').UserError;
+var UserOk = require('../error').UserOk;
+var BasicClass = require('./system/BasicClass');
+var util = require('util');
+var async = require('async');
+var rollback = require('../modules/rollback');
+
+var Model = function(obj){
+    this.name = obj.name;
+    this.tableName = obj.name.toLowerCase();
+
+    var basicclass = BasicClass.call(this, obj);
+    if (basicclass instanceof MyError) return basicclass;
+};
+util.inherits(Model, BasicClass);
+Model.prototype.addPrototype = Model.prototype.add;
+
+Model.prototype.init = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    if (typeof cb !== 'function') throw new MyError('В метод не передан cb');
+    if (typeof obj !== 'object') return cb(new MyError('В метод не переданы obj'));
+    var _t = this;
+    Model.super_.prototype.init.apply(this, [obj , function (err) {
+        cb(null);
+    }]);
+};
+
+Model.prototype.add = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    var _t = this;
+    var client_object = _t.client_object || '';
+
+    var coFunction = 'add_' + client_object;
+    if (typeof _t[coFunction] === 'function') {
+        _t[coFunction](obj, cb);
+    } else {
+        if (typeof _t['add_'] === 'function') {
+            _t['add_'](obj, cb);
+        } else {
+            _t.addPrototype(obj, cb);
+        }
+    }
+};
+
+module.exports = Model;
