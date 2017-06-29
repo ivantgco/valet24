082bc00e2e244aa24f9622048597f024f523c569
alextgco <alextgco@gmail.com>
2017-06-30 02:19:23 +0300

    отзывы начало акций crm

---
+++ classes/Review.js
@@ -0,0 +1,148 @@
+/**
+ * Created by iig on 29.10.2015.
+ */
+var MyError = require('../error').MyError;
+var UserError = require('../error').UserError;
+var UserOk = require('../error').UserOk;
+var BasicClass = require('./system/BasicClass');
+var util = require('util');
+var async = require('async');
+var rollback = require('../modules/rollback');
+var funcs = require('../libs/functions');
+
+var Model = function(obj){
+    this.name = obj.name;
+    this.tableName = obj.name.toLowerCase();
+
+    var basicclass = BasicClass.call(this, obj);
+    if (basicclass instanceof MyError) return basicclass;
+};
+util.inherits(Model, BasicClass);
+Model.prototype.getPrototype = Model.prototype.get;
+Model.prototype.addPrototype = Model.prototype.add;
+Model.prototype.modifyPrototype = Model.prototype.modify;
+Model.prototype.removeCascadePrototype = Model.prototype.removeCascade;
+
+Model.prototype.init = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    if (typeof cb !== 'function') throw new MyError('В метод не передан cb');
+    if (typeof obj !== 'object') return cb(new MyError('В метод не переданы obj'));
+    var _t = this;
+    Model.super_.prototype.init.apply(this, [obj , function (err) {
+        cb(null);
+    }]);
+};
+
+Model.prototype.get = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    var _t = this;
+    var client_object = _t.client_object || '';
+
+    var coFunction = 'get_' + client_object;
+    if (typeof _t[coFunction] === 'function') {
+        _t[coFunction](obj, cb);
+    } else {
+        if (typeof _t['get_'] === 'function') {
+            _t['get_'](obj, cb);
+        } else {
+            _t.getPrototype(obj, cb);
+        }
+    }
+};
+
+Model.prototype.add = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    var _t = this;
+    var client_object = _t.client_object || '';
+
+    var coFunction = 'add_' + client_object;
+    if (typeof _t[coFunction] === 'function') {
+        _t[coFunction](obj, cb);
+    } else {
+        if (typeof _t['add_'] === 'function') {
+            _t['add_'](obj, cb);
+        } else {
+            _t.addPrototype(obj, cb);
+        }
+    }
+};
+
+Model.prototype.modify = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    var _t = this;
+    var client_object = _t.client_object || '';
+
+    var coFunction = 'modify_' + client_object;
+
+    if (typeof _t[coFunction] === 'function') {
+        _t[coFunction](obj, cb);
+    } else {
+        if (typeof _t['modify_'] === 'function') {
+            _t['modify_'](obj, cb);
+        } else {
+            _t.modifyPrototype(obj, cb);
+        }
+    }
+};
+
+Model.prototype.removeCascade = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    var _t = this;
+    var client_object = _t.client_object || '';
+
+    var coFunction = 'removeCascade_' + client_object;
+
+    if (typeof _t[coFunction] === 'function') {
+        _t[coFunction](obj, cb);
+    } else {
+        if (typeof _t['removeCascade_'] === 'function') {
+            _t['removeCascade_'](obj, cb);
+        } else {
+            _t.removeCascadePrototype(obj, cb);
+        }
+    }
+};
+
+Model.prototype.example = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    var _t = this;
+    var id = obj.id;
+    if (isNaN(+id)) return cb(new MyError('Не передан id',{obj:obj}));
+    var rollback_key = obj.rollback_key || rollback.create();
+
+    async.series({
+
+    },function (err, res) {
+        if (err) {
+            if (err.message == 'needConfirm') return cb(err);
+            rollback.rollback({rollback_key: rollback_key, user: _t.user}, function (err2) {
+                return cb(err, err2);
+            });
+        } else {
+            //if (!obj.doNotSaveRollback){
+            //    rollback.save({rollback_key:rollback_key, user:_t.user, name:_t.name, name_ru:_t.name_ru || _t.name, method:'METHOD_NAME', params:obj});
+            //}
+            cb(null, new UserOk('Ок'));
+        }
+    });
+}
+
+module.exports = Model;
